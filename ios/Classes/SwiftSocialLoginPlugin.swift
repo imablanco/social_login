5ae17efa1d31897c42cacf2328b01a69643901d4
Luis Páez González <luis.paez@wembleystudios.com>
2018-11-13 01:12:54 +0100

    iOS: Add social networks Facebook, Twitter and Google

--- ios/Classes/SwiftSocialLoginPlugin.swift
+++ ios/Classes/SwiftSocialLoginPlugin.swift
@@ -1,6 +1,17 @@
 import Flutter
 import UIKit
 
+
+enum Method: String {
+    case LOGIN_FACEBOOK = "login_facebook"
+    case LOGOUT_FACEBOOK = "logout_facebook"
+    case LOGIN_GOOGLE = "login_google"
+    case LOGOUT_GOOGLE = "logout_google"
+    case LOGIN_TWITTER = "login_twitter"
+    case LOGOUT_TWITTER = "logout_twitter"
+}
+
+
 public class SwiftSocialLoginPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "social_login", binaryMessenger: registrar.messenger())
@@ -9,6 +20,36 @@ public class SwiftSocialLoginPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    //result("iOS " + UIDevice.current.systemVersion)
+    
+    switch call.method {
+        
+    case Method.LOGIN_FACEBOOK.rawValue:
+        
+        break
+        
+    case Method.LOGOUT_FACEBOOK.rawValue:
+        
+        break
+        
+    case Method.LOGIN_GOOGLE.rawValue:
+    
+        break
+        
+    case Method.LOGOUT_GOOGLE.rawValue:
+        
+        break
+        
+    case Method.LOGIN_TWITTER.rawValue:
+        
+        break
+        
+    case Method.LOGOUT_TWITTER.rawValue:
+        
+        break
+        
+    default: break
+        
+    }
   }
 }
